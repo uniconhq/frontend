a3349ff8bf5bbebe69d0e35fae4c605d4aa31d5c
Shen Yi Hong <shen_yi_hong@outlook.com>
2025-02-24 18:34:20 +0800

    feat: add "Lock Date" to problems table

--- src/features/problems/table/columns.tsx
+++ src/features/problems/table/columns.tsx
@@ -1,14 +1,14 @@
-import { ColumnDef } from "@tanstack/react-table";
-import { Info, PencilIcon, ScanIcon } from "lucide-react";
+import { BanIcon, ExpandIcon, InfoIcon, PenLineIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 
 import { ProblemBaseWithPermissions } from "@/api";
+import { ExtendedColumnDef } from "@/components/ui/data-table";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 import { DraftBadge, RestrictedBadge } from "@/features/problems/components/badges";
 import { cn } from "@/lib/utils";
 import { formatDateShort } from "@/utils/date";
 
-export const columns: ColumnDef<ProblemBaseWithPermissions>[] = [
+export const columns: ExtendedColumnDef<ProblemBaseWithPermissions>[] = [
   {
     header: "Name",
     cell: ({ row }) => {
@@ -22,16 +22,17 @@ export const columns: ColumnDef<ProblemBaseWithPermissions>[] = [
     },
   },
   {
-    header: "Starts at",
+    header: "Release Date",
+    tooltip: "The date when the problem is released and submissions are accepted.",
     cell: ({ row }) => {
       const isOpen = new Date(row.original.started_at) <= new Date();
       return (
         <div className="flex items-center gap-2">
-          <span className={cn(!isOpen && "font-bold")}>{formatDateShort(row.original.started_at)}</span>
+          <span className={cn(!isOpen && "font-medium")}>{formatDateShort(row.original.started_at)}</span>
           {!isOpen && (
             <Tooltip>
               <TooltipTrigger asChild>
-                <Info className="h-4 w-4 cursor-pointer" />
+                <InfoIcon className="h-4 w-4 text-zinc-500" />
               </TooltipTrigger>
               <TooltipContent>
                 <p>This problem is not available yet.</p>
@@ -43,9 +44,28 @@ export const columns: ColumnDef<ProblemBaseWithPermissions>[] = [
     },
   },
   {
-    header: "Ends at",
+    header: "Due Date",
+    tooltip: "The date when the problem is due, after which submissions will be considered late.",
     cell: ({ row }) => {
       return formatDateShort(row.original.ended_at);
+    },
+  },
+  {
+    header: "Lock Date",
+    tooltip: "The date when the problem is locked and no longer accepting submissions.",
+    cell: ({ row }) => {
+      return row.original.closed_at ? (
+        formatDateShort(row.original.closed_at)
+      ) : (
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <BanIcon className="h-4 w-4 text-zinc-500" />
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>No lock date set, problem will always be open for submissions</p>
+          </TooltipContent>
+        </Tooltip>
+      );
     },
   },
   {
@@ -54,13 +74,13 @@ export const columns: ColumnDef<ProblemBaseWithPermissions>[] = [
     cell: ({ row }) => {
       const id = row.original.id;
       return (
-        <div className="flex gap-2">
+        <div className="flex items-center gap-2">
           {row.original.view && (
             <Link
               to={`/projects/${row.original.project_id}/problems/${id}`}
               className="hover:text-purple-300 hover:underline"
             >
-              <ScanIcon />
+              <ExpandIcon size={18} />
             </Link>
           )}
           {row.original.edit && (
@@ -68,7 +88,7 @@ export const columns: ColumnDef<ProblemBaseWithPermissions>[] = [
               to={`/projects/${row.original.project_id}/problems/${id}/edit`}
               className="hover:text-purple-300 hover:underline"
             >
-              <PencilIcon />
+              <PenLineIcon size={18} />
             </Link>
           )}
         </div>
